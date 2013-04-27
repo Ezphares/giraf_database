fe0cba5970d321558301a2018d178de90a5e6cf0
Ezphares <raider.j@gmail.com>
2013-04-27 15:43:39 +0200

    Started API module

---
+++ source/server/api/api.cpp
@@ -0,0 +1,139 @@
+/*
+ * API.cpp
+ */
+
+#include "api.h"
+#include <json/reader.h>
+#include <json/writer.h>
+#include <cstring>
+
+API::API() {
+	// TODO Auto-generated constructor stub
+
+}
+
+API::~API() {
+	// TODO Auto-generated destructor stub
+}
+
+
+/*
+ * NOTE ON GOTO:
+ * While GOTO is normally a bad idea, in this particular case it is the only sensible way to avoid
+ * the code becoming a mess. We know and acknowledge that you should be VERY careful with gotos,
+ * and all other options were considered.
+ */
+
+const char *API::handle_request(const char *json)
+{
+	Json::Value root, response(Json::objectValue), errors(Json::arrayValue);
+	Json::Reader reader;
+	Json::FastWriter writer;
+
+	response["errors"] = errors;
+	response["data"] = Json::Value(Json::nullValue);
+	response["status"] = Json::Value("OK");
+
+	if (!reader.parse(json, root))
+	{
+		// Parsing failed
+		response["status"] = Json::Value("SYNTAXERROR");
+		errors.append(Json::Value("Request was not valid JSON"));
+
+		goto done;
+	}
+
+	if (validate_top(root, errors) < 0)
+	{
+		response["status"] = Json::Value("BADREQUEST");
+
+		goto done;
+	}
+
+	if (root["action"].isNull())
+	{
+		// TODO
+	}
+	else
+	{
+		const char* action = root["action"].asCString();
+		if (strcmp(action, "create") == 0)
+		{
+			//TODO
+		}
+		else if (strcmp(action, "read") == 0)
+		{
+			//TODO
+		}
+		else if (strcmp(action, "update") == 0)
+		{
+			//TODO
+		}
+		else if (strcmp(action, "delete") == 0)
+		{
+			//TODO
+		}
+		else
+		{
+			errors.append("\"action\" was not null or a CRUD action");
+			response["status"] = "BADREQUEST";
+
+			goto done;
+		}
+	}
+
+	done:
+	response["errors"] = errors;
+	return writer.write(response).c_str();
+}
+
+
+int API::validate_top(Json::Value &request, Json::Value &errors)
+{
+	if (!request.isObject()) // This check SHOULD be redundant, due to json syntax, but just to be safe.
+	{
+		errors.append(Json::Value("Request was not an object"));
+	}
+
+	if (!request.isMember("auth"))
+	{
+		errors.append(Json::Value("\"auth\" key missing"));
+	}
+	else
+	{
+		if (!request["auth"].isObject())
+		{
+			errors.append(Json::Value("\"auth\" was not an object"));
+		}
+	}
+
+	if (!request.isMember("action"))
+	{
+		errors.append(Json::Value("\"action\" key missing"));
+	}
+	else
+	{
+		if (!request["action"].isString() && !request["action"].isNull())
+		{
+			errors.append(Json::Value("\"action\" was not an a string or null"));
+		}
+	}
+
+	if (!request.isMember("data"))
+	{
+		errors.append(Json::Value("\"data\" key missing"));
+	}
+	else
+	{
+		if (!request["data"].isObject())
+		{
+			errors.append(Json::Value("\"data\" was not an object"));
+		}
+	}
+
+	if (errors.empty()) return 0;
+	else return -1;
+}
+
+
+
---
+++ source/server/api/api.h
@@ -0,0 +1,56 @@
+/*
+ * API.h
+ */
+
+#pragma once
+
+enum rights_t : char {CREATE = 0, READ = 1, UPDATE = 2, DELETE = 3};
+enum rights_v : char {NONE = 0, EMULATE = 1, TRUE = 3};
+
+#include <json/value.h>
+#include <vector>
+#include <map>
+
+class API
+{
+public:
+	API();
+	virtual ~API();
+
+public:
+	const char *handle_request(const char *json);
+
+private:
+	// RETURN:
+	// >0: User ID
+	// -1: Not authenticated
+	// -2: Missing key/wrong value-type
+	int authenticate(Json::Value &auth);
+
+	int authenticate_user_pass(Json::Value &username, Json::Value &password);
+	int authenticate_certificate(Json::Value &certificate);
+	// int authenticate_session(Json::Value session_key); // TODO Future work
+
+
+	int validate_top(Json::Value &request, Json::Value &errors);
+
+
+
+
+	std::map<int, rights_v> get_rights(int user_id, const char *data_type, std::vector<int> ids, rights_t rights);
+
+	/*
+	 * ALL API CALLS GO HERE:
+	 */
+
+	Json::Value read_profile_list(Json::Value &request_data);
+	Json::Value read_profile_details(Json::Value &request_data);
+	Json::Value update_profile(Json::Value &request_data);
+	Json::Value create_profile(Json::Value &request_data);
+	Json::Value delete_profile(Json::Value &request_data);
+	// ETC, ETC
+
+
+
+};
+
