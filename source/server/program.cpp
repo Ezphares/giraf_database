2ba08a1e743148f1e15d059c09719839b7801a5e
Ezphares <raider.j@gmail.com>
2013-04-27 15:43:39 +0200

    Started API module

--- source/server/program.cpp
+++ source/server/program.cpp
@@ -1,4 +1,47 @@
-int main(int argc, char *argv[])
+
+#include "connection/framework.h"
+#include "api/api.h"
+#include <iostream>
+
+
+
+int handle_api(Connection *connection)
 {
+	const char *request = connection->receive();
+	const char *response = (new API())->handle_request(request);
+	connection->send(response);
+	return 0;
+}
+
+
+
+
+
+int main (int argc, char *argv[])
+{
+	char api[1024];
+
+	while (api[0] != 's')
+	{
+		std::cin.getline(api, 1023);
+		std::cout << API().handle_request(api) << std::endl;
+	}
+
+
+
+
+	/*
+	ServerInfo *server = run_server(666, handle_api);
+
+	char *a = "";
+
+	while (a[0] != 's')
+	{
+		std::cin >> a;
+	}
+
+	stop_server(server);
+	*/
+
 	return 0;
 }
