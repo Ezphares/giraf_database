62cd9b15d723ca849637871ba7219ec6c7e31447
Ezphares <raider.j@gmail.com>
2013-05-01 16:42:32 +0200

    More list scripts

---
+++ source/server/api/sql/pictogram_read_list.sql
@@ -0,0 +1,20 @@
+SET @user=%d;
+SELECT DISTINCT `profile_pictogram`.`pictogram_id` AS `id`
+	FROM
+		`profile`
+		JOIN
+		`profile_pictogram`
+		ON `profile`.`id`=`profile_pictogram`.`profile_id`
+	WHERE `profile`.`user_id`=@user
+UNION
+SELECT DISTINCT `department_pictogram`.`pictogram_id` AS `id`
+	FROM
+		(`profile`
+		JOIN
+		`department`
+		ON `profile`.`department_id`=`department`id)
+		JOIN
+		`department_pictogram`
+		ON `department`.`id`=`department_pictogram`.`department_id`
+	WHERE `profile`.`user_id`=@user;
+
