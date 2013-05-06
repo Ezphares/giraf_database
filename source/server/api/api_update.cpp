c6c870fcb6c69ca74629dcd3608c82d7a65ea056
Ezphares <raider.j@gmail.com>
2013-05-06 14:13:51 +0200

    Validation

--- source/server/api/api_update.cpp
+++ source/server/api/api_update.cpp
@@ -8,11 +8,101 @@
 
 int API::validate_update(Json::Value &data, Json::Value &errors)
 {
-	return 0;
+	if (!data.isMember("type"))
+	{
+		errors.append(Json::Value("\"data\":\"type\" key missing"));
+	}
+	else
+	{
+		if (!data["type"].isString())
+		{
+			errors.append(Json::Value("\"data\":\"type\" was not a string"));
+		}
+	}
+
+	if (!data.isMember("values"))
+	{
+		errors.append(Json::Value("\"data\":\"values\" key missing"));
+		if (!data["values"].isArray())
+		{
+			errors.append(Json::Value("\"data\":\"values\" was not an array"));
+		}
+		else
+		{
+			for (unsigned int i = 0; i < data["values"].size(); i++)
+			{
+				Json::Value &object = data["values"][i];
+				if (!object.isMember("id"))
+				{
+					errors.append(Json::Value("Missing update id"));
+				}
+				else
+				{
+					if (object["id"].isInt())
+					{
+						errors.append(Json::Value("Update id was not an integer"));
+					}
+				}
+
+				if (!object.isMember("value"))
+				{
+					errors.append(Json::Value("Missing update value"));
+				}
+				else
+				{
+					if (object["value"].isObject())
+					{
+						errors.append(Json::Value("Update value was not an object"));
+					}
+				}
+
+			}
+		}
+	}
+
+	if (errors.empty()) return 0;
+	else return -1;
 }
 
-int API::api_update(Json::Value &data, Json::Value &response, Json::Value &errors)
+int API::api_update(Json::Value &request, Json::Value &response, Json::Value &errors)
 {
+	if (validate_update(request["data"], errors) < 0)
+	{
+		response["status"] = Json::Value(STATUS_STRUCTURE);
+		return -1;
+	}
+
+	int user = authenticate(request["auth"]);
+
+	if (user == -1)
+	{
+		response["status"] = STATUS_AUTH;
+		errors.append(Json::Value("Authentication failed"));
+		return -1;
+	}
+	create_session(response, user);
+
+	Json::Value call_data;
+
+	if (strcmp(request["data"]["type"].asCString(), "profile") == 0) 			call_data;//TODO = update_profile(request["data"], user, errors);
+	else if (strcmp(request["data"]["type"].asCString(), "department") == 0) 	call_data;//TODO = update_department(request["data"], user, errors);
+	else if (strcmp(request["data"]["type"].asCString(), "user") == 0) 			call_data;//TODO = update_user(request["data"], user, errors);
+	else if (strcmp(request["data"]["type"].asCString(), "pictogram") == 0) 	call_data;//TODO = delete_pictogram(request["data"], user, errors);
+	else if (strcmp(request["data"]["type"].asCString(), "application") == 0) 	call_data;//TODO = update_application(request["data"], user, errors);
+	else
+	{
+		response["status"] = STATUS_STRUCTURE;
+		errors.append(Json::Value("Invalid data type requested"));
+	}
+
+	if (!errors.empty())
+	{
+		response["status"] = Json::Value(STATUS_ACCESS);
+		return -1;
+	}
+
+	response["data"] = call_data;
+
 	return 0;
 }
 
