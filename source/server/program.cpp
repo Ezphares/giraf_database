2cc96927debccafc40de476b07cb498f03b9572c
Jeppe Tarp <raider.j@gmail.com>
2013-05-07 11:03:55 +0200

    Fixed memory allocation

--- source/server/program.cpp
+++ source/server/program.cpp
@@ -7,9 +7,11 @@
 
 int handle_api(Connection *connection)
 {
-	const char *request = connection->receive();
+	char *request = connection->receive();
 	std::cout << request << std::endl;
 	std::string response = (new API())->handle_request(request);
+
+	delete[] request;
 	std::cout << response.c_str() << std::endl;
 	connection->send(response.c_str());
 	connection->disconnect();
