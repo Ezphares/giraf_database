6a308133c30d8904be5aa64d892442227d7f86c1
DorkMatter <barbaraalbertine@gmail.com>
2013-05-13 23:19:25 +0200

    linking done, no test

--- source/server/api/api_link.cpp
+++ source/server/api/api_link.cpp
@@ -125,12 +125,117 @@ int API::api_link(Json::Value &request, Json::Value &response, Json::Value &erro
 
 Json::Value API::execute_link(Json::Value &data, int user, Json::Value &errors)
 {
+	int profile = 0;
+	int department = 0;
 	char query[API_BUFFER_SIZE];
-	snprintf(query, API_BUFFER_SIZE, "SELECT DISTINCT `id`, `name`, `role`, MAX(`update`) AS `update`, MAX(`delete`) AS `delete` FROM `profile_list` WHERE `user_id`=%d GROUP BY `id`;", user);
-	QueryResult *result = _database->send_query(query);
+	QueryResult *result;
+	row_t r;
 
-	Json::Value call_data = build_array_from_query(result, fix_profile_list);
+	if(data.isMember("profile"))
+	{
+		char query[API_BUFFER_SIZE];
+		snprintf(query, API_BUFFER_SIZE, "SELECT DISTINCT `id` FROM `profile_list` WHERE `user_id`=%d AND `update`=1;", user);
+		result = _database->send_query(query);
+		std::vector<int> accessible = build_simple_int_vector_from_query(result, "id");
+		delete result;
 
-	delete result;
-	return call_data;
+		profile = data["profile"].asInt();
+		if (!validate_value_in_vector(profile, accessible))
+		{
+			errors.append(Json::Value("Illegal profile"));
+			return Json::Value(Json::nullValue);
+		}
+	}
+
+	if(data.isMember("department"))
+	{
+		snprintf(query, API_BUFFER_SIZE, "SELECT DISTINCT `id` FROM `department_list` WHERE `user_id`=%d AND `update`=1;", user);
+		result = _database->send_query(query);
+		std::vector<int> accessible = build_simple_int_vector_from_query(result, "id");
+		delete result;
+
+		department = data["department"].asInt();
+		if (!validate_value_in_vector(department, accessible))
+		{
+			errors.append(Json::Value("Illegal department"));
+			return Json::Value(Json::nullValue);
+		}
+	}
+
+	int actor_id = profile + department;
+	const char *actor = profile != 0 ? "profile" : "department";
+
+	if(data.isMember("link"))
+	{
+		for(unsigned int i = 0; i < data["link"].size(); i++)
+		{
+			Json::Value &object = data["link"][i];
+			char *settings = NULL;
+			char *type;
+			_database->escape(type, object["type"].asCString());
+			int object_id = object["id"].asInt();
+
+			if(strcmp(object["type"].asCString(), "application") == 0)
+			{
+				if(object.isMember("settings") && profile != 0)	_database->escape(settings, object["settings"].asCString());
+
+			}
+			else
+			{
+				//TODO: we need to update the view to include public pictograms!
+				/*
+				snprintf(query, API_BUFFER_SIZE, "SELECT DISTINCT `id` FROM `pictogram_list` WHERE `user_id`=%d OR `public`=1;", user);
+				result = _database->send_query(query);
+				std::vector<int> accessible = build_simple_int_vector_from_query(result, "id");
+				delete result;
+
+				int pictogram = data["id"].asInt();
+				if (!validate_value_in_vector(pictogram, accessible))
+				{
+					errors.append(Json::Value("Illegal pictogram"));
+					return Json::Value(Json::nullValue);
+				}
+				*/
+			}
+
+			snprintf(query, API_BUFFER_SIZE, "SELECT * FROM `%s_%s` WHERE `%s_id`=%d AND `%s_id`=%d;", actor, type, actor, actor_id, type, object_id);
+			result = _database->send_query(query);
+			r = result->next_row();
+			delete result;
+
+			if (!r.empty())
+			{
+				if (settings == NULL) continue;
+
+				snprintf(query, API_BUFFER_SIZE, "UPDATE `%s_%s` SET `settings`='%s' WHERE `%s_id`=%d AND `%s_id`=%d;", actor, type, settings, actor, actor_id, type, object_id);
+				result = _database->send_query(query);
+				delete result;
+			}
+			else
+			{
+				if (settings == NULL)
+					snprintf(query, API_BUFFER_SIZE, "INSERT INTO `%s_%s` (`%s_id`, `%s_id`) VALUES (%d, %d);", actor, type, actor, type, actor_id, object_id);
+				else
+					snprintf(query, API_BUFFER_SIZE, "INSERT INTO `%s_%s` (`%s_id`, `%s_id`, `settings`) VALUES (%d, %d, '%s');", actor, type, actor, type, actor_id, object_id, settings);
+				result = _database->send_query(query);
+				delete result;
+			}
+		}
+	}
+
+	if(data.isMember("unlink"))
+	{
+		for(unsigned int i = 0; i < data["link"].size(); i++)
+		{
+			Json::Value &object = data["link"][i];
+			char *type;
+			_database->escape(type, object["type"].asCString());
+			int object_id = object["id"].asInt();
+
+			snprintf(query, API_BUFFER_SIZE, "DELETE FROM `%s_%s` WHERE `%s_id`=%d AND `%s_id`=%d;", actor, type, actor, actor_id, type, object_id);
+			result = _database->send_query(query);
+			delete result;
+		}
+	}
+	return Json::Value(Json::nullValue);
 }
